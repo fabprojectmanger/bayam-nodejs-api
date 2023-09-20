439029098074ecf855ca2f30103ff76f5392d559
fabprojectmanger <fabcodemanager@gmail.com>
2023-09-20 12:32:44 +0530

    Fixed issues of API's Responses

--- functions/services/discount.service.js
+++ functions/services/discount.service.js
@@ -5,21 +5,25 @@ async function getDiscountCouponDetails({ code }, user) {
 
 		let allPriceRules = await shopifyService.getCall('price_rules.json?status=active&limit=250');
 		let filterPrice = allPriceRules.price_rules.filter(ele => ele.title === code)[0];
-		return {
-			value: filterPrice.value,
-			valueType: filterPrice.value_type,
-			title: filterPrice.title,
-			productIds: filterPrice.prerequisite_product_ids,
-			variantIds: filterPrice.prerequisite_variant_ids,
-			subTotalRange: filterPrice?.prerequisite_subtotal_range?.greater_than_or_equal_to,
-			allocationLimit: filterPrice.allocation_limit,
-			oncePerCustomer: filterPrice.once_per_customer,
-			usageLimit: filterPrice.usage_limit,
-			startAt: filterPrice.starts_at,
-			endsAt: filterPrice.ends_at,
-			createdAt: filterPrice.created_at,
-			updatedAt: filterPrice.updated_at
-		};
+		if (filterPrice) {
+			return {
+				value: filterPrice.value,
+				valueType: filterPrice.value_type,
+				title: filterPrice.title,
+				productIds: filterPrice.prerequisite_product_ids,
+				variantIds: filterPrice.prerequisite_variant_ids,
+				subTotalRange: filterPrice?.prerequisite_subtotal_range?.greater_than_or_equal_to,
+				allocationLimit: filterPrice.allocation_limit,
+				oncePerCustomer: filterPrice.once_per_customer,
+				usageLimit: filterPrice.usage_limit,
+				startAt: filterPrice.starts_at,
+				endsAt: filterPrice.ends_at,
+				createdAt: filterPrice.created_at,
+				updatedAt: filterPrice.updated_at
+			};
+		} else {
+			return {};
+		}
 	} catch (e) {
 		throw e;
 	}
