1f1d2173909f3cfda4f12c0a3146382ef37c3d62
fabprojectmanger <fabcodemanager@gmail.com>
2023-09-28 19:21:51 +0530

    Clean File

--- functions/controllers/stripePayment.controller.js
+++ functions/controllers/stripePayment.controller.js
@@ -1,73 +1,42 @@
+let puppeteer = require('puppeteer');
 let express = require('express');
 let router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.post('/', async function (req, res) {
-  const { paymentMethodId } = req.body;
-
+  const { paymentMethodId, customerName, line1, city, state, postalCode, country } = req.body;
   try {
-    // Create a payment intent with the token
+    const customer = await stripe.customers.create({
+      name: customerName,
+      address: {
+        line1: line1,
+        city: city,
+        state: state,
+        postal_code: postalCode,
+        country: country,
+      }
+    });
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 3, // The amount in cents (e.g., $10.00)
+      amount: 3,
       currency: 'USD',
       description: 'Example Payment',
       payment_method_types: ['card'],
       payment_method: paymentMethodId,
       confirm: true,
-      shipping: {
-        name: 'John Doe', // Customer's name
-        address: {
-          line1: '123 Main Street',
-          city: 'City',
-          state: 'State',
-          postal_code: '12345',
-          country: 'IN', // India
-        }
-      }
+      customer:customer.id
     });
+    if (paymentIntent && paymentIntent.next_action) {
+      let url = paymentIntent.next_action.use_stripe_sdk.stripe_js;
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(url);
+      await browser.close();
 
-
-    // If the payment was successful, you can send a success response
+    }
     res.json({ success: true, paymentIntent });
-  }catch(error){
+  } catch (error) {
     throw error;
   }
 });
-
-// router.post('/', async function (req, res) {
-//   console.log("Request Body------------>",req.body);
-//     const { name, cardNumber, expirationDate, securityCode, amount, currency } = req.body;
-//     try {
-//       // Create a Payment Intent using card details
-//       const paymentIntent = await stripe.paymentIntents.create({
-//         amount,
-//         currency,
-//         description: 'Example Payment',
-//         payment_method_data: {
-//           type: 'card',
-//           card: {
-//             number: cardNumber,
-//             exp_month: expirationDate.split('/')[0],
-//             exp_year: expirationDate.split('/')[1],
-//             cvc:securityCode,
-//           },
-//         },
-//         confirm: true,
-//         automatic_payment_methods: {
-//             enabled: true,
-//             allow_redirects: 'never',
-//           },
-//       });
-  
-//       // Payment successful
-//       res.json({ success: true, message: 'Payment succeeded' });
-//     } catch (error) {
-//       // Handle payment failure
-//       console.error(error);
-//       res.status(500).json({ success: false, error: error.message });
-//     }
-  
-// });
-
 
 module.exports = router;
